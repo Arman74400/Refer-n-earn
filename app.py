f7c0f5b1a8b72b0688aff1908375e2b464452354
Arman74400 <146393881+Arman74400@users.noreply.github.com>
2024-09-19 02:55:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return ' I Am alive'
+    return 'unicornguardian'
+
 
 if __name__ == "__main__":
     app.run()
